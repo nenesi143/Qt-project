a8e69ecd73d8fe5b39dfe48d4a3c96ee1f9a9021
Matvey Khanzhin <motyalove17@mail.ru>
2025-05-02 21:40:55 +0300

    Добавил .exe приложение для Windows

---
+++ LabWorkManager/test1_autogen/EWIEGA46WW/moc_mainwindow.cpp
@@ -0,0 +1,191 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.12)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../123/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.12. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[19];
+    char stringdata0[376];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 19), // "on_Diagram3_clicked"
+QT_MOC_LITERAL(2, 31, 0), // ""
+QT_MOC_LITERAL(3, 32, 21), // "on_addLabwork_clicked"
+QT_MOC_LITERAL(4, 54, 23), // "on_ImportButton_clicked"
+QT_MOC_LITERAL(5, 78, 23), // "on_DeleteRecord_clicked"
+QT_MOC_LITERAL(6, 102, 23), // "on_ExportButton_clicked"
+QT_MOC_LITERAL(7, 126, 25), // "on_DeleteRecord_2_clicked"
+QT_MOC_LITERAL(8, 152, 21), // "on_EditRecord_clicked"
+QT_MOC_LITERAL(9, 174, 20), // "on_newColumn_clicked"
+QT_MOC_LITERAL(10, 195, 19), // "on_Request1_clicked"
+QT_MOC_LITERAL(11, 215, 19), // "on_Request2_clicked"
+QT_MOC_LITERAL(12, 235, 19), // "on_Request3_clicked"
+QT_MOC_LITERAL(13, 255, 19), // "on_Request4_clicked"
+QT_MOC_LITERAL(14, 275, 19), // "on_Request5_clicked"
+QT_MOC_LITERAL(15, 295, 21), // "on_Request2_2_clicked"
+QT_MOC_LITERAL(16, 317, 19), // "on_Diagram1_clicked"
+QT_MOC_LITERAL(17, 337, 19), // "on_Diagram2_clicked"
+QT_MOC_LITERAL(18, 357, 18) // "on_graphic_clicked"
+
+    },
+    "MainWindow\0on_Diagram3_clicked\0\0"
+    "on_addLabwork_clicked\0on_ImportButton_clicked\0"
+    "on_DeleteRecord_clicked\0on_ExportButton_clicked\0"
+    "on_DeleteRecord_2_clicked\0"
+    "on_EditRecord_clicked\0on_newColumn_clicked\0"
+    "on_Request1_clicked\0on_Request2_clicked\0"
+    "on_Request3_clicked\0on_Request4_clicked\0"
+    "on_Request5_clicked\0on_Request2_2_clicked\0"
+    "on_Diagram1_clicked\0on_Diagram2_clicked\0"
+    "on_graphic_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      17,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   99,    2, 0x08 /* Private */,
+       3,    0,  100,    2, 0x08 /* Private */,
+       4,    0,  101,    2, 0x08 /* Private */,
+       5,    0,  102,    2, 0x08 /* Private */,
+       6,    0,  103,    2, 0x08 /* Private */,
+       7,    0,  104,    2, 0x08 /* Private */,
+       8,    0,  105,    2, 0x08 /* Private */,
+       9,    0,  106,    2, 0x08 /* Private */,
+      10,    0,  107,    2, 0x08 /* Private */,
+      11,    0,  108,    2, 0x08 /* Private */,
+      12,    0,  109,    2, 0x08 /* Private */,
+      13,    0,  110,    2, 0x08 /* Private */,
+      14,    0,  111,    2, 0x08 /* Private */,
+      15,    0,  112,    2, 0x08 /* Private */,
+      16,    0,  113,    2, 0x08 /* Private */,
+      17,    0,  114,    2, 0x08 /* Private */,
+      18,    0,  115,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_Diagram3_clicked(); break;
+        case 1: _t->on_addLabwork_clicked(); break;
+        case 2: _t->on_ImportButton_clicked(); break;
+        case 3: _t->on_DeleteRecord_clicked(); break;
+        case 4: _t->on_ExportButton_clicked(); break;
+        case 5: _t->on_DeleteRecord_2_clicked(); break;
+        case 6: _t->on_EditRecord_clicked(); break;
+        case 7: _t->on_newColumn_clicked(); break;
+        case 8: _t->on_Request1_clicked(); break;
+        case 9: _t->on_Request2_clicked(); break;
+        case 10: _t->on_Request3_clicked(); break;
+        case 11: _t->on_Request4_clicked(); break;
+        case 12: _t->on_Request5_clicked(); break;
+        case 13: _t->on_Request2_2_clicked(); break;
+        case 14: _t->on_Diagram1_clicked(); break;
+        case 15: _t->on_Diagram2_clicked(); break;
+        case 16: _t->on_graphic_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 17)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 17;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 17)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 17;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
