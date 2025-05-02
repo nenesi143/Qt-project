ba1a84d236edc8396a7e8e88e8a6ac9939a3a9aa
nenesihuynu? <motyalove17@mail.ru>
2025-05-02 22:27:05 +0300

    Delete LabWorkManager directory

--- LabWorkManager/qtcsettings.cmake
+++
@@ -1,8 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "MinSizeRel" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "F:/qt/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "F:/qt/5.12.12/mingw73_64/bin/qmake.exe" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "F:/qt/5.12.12/mingw73_64" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "F:/qt/Tools/mingw730_64/bin/gcc.exe" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_COMPILER" "F:/qt/Tools/mingw730_64/bin/g++.exe" CACHE "STRING" "" FORCE)
